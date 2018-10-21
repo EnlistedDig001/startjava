d793d0a620fcab04b5779a0b7c2df2ab70fc87ea
EnlistedDig001 <vsokolov0011@gmail.com>
2018-10-21 13:38:54 +0300

    Players initialization moved out of loop

--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -4,18 +4,18 @@ public class GuessNumberTest {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String userAnswer;
-		Player player1;
-		Player player2;
+
+		System.out.println("\n" + "Enter first player name:");
+		Player player1 = new Player(scan.nextLine());	
+
+		System.out.println("Enter second player name:");
+		Player player2 = new Player(scan.nextLine());	
+
 		GuessNumber guessNumber = new GuessNumber(player1, player2);
 
 		do {
-			System.out.println("\n" + "Enter first player name:");
-			player1 = new Player(scan.nextLine());	
-
-			System.out.println("Enter second player name:");
-			player2 = new Player(scan.nextLine());	
-
 			guessNumber.startGame();
+			
 			do {
 				System.out.println("\n" + "Do you want to start over ? y/n");
 				userAnswer = scan.nextLine();
