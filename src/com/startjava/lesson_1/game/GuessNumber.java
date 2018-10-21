3d095c30e8db219f8a2892d46b287d88852941f9
EnlistedDig001 <vsokolov0011@gmail.com>
2018-10-21 22:04:51 +0300

    First lesson game rewrited

---
+++ src/com/startjava/lesson_1/game/GuessNumber.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class GuessNumber {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Random rand = new Random();
+		int userGuessNum;
+
+		System.out.println("Компьютер загадал число, Вы должны угадать его.");
+		int computerGuessNum = rand.nextInt(100);
+
+		do {
+			System.out.println("Ваше число:");
+			userGuessNum = scan.nextInt();
+
+			if (userGuessNum < computerGuessNum) {
+				System.out.println("Ваше число меньше загаданного.");			
+			} else if (userGuessNum > computerGuessNum) {
+				System.out.println("Ваше число больше загаданного.");
+		
+			}
+		} while (userGuessNum != computerGuessNum);
+		
+		System.out.println("Вы правы.");	
+	}
+}
