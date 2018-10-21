9fb14073f06e3b5af0a29e0ef5ce1fda5ea0d9f9
EnlistedDig001 <vsokolov0011@gmail.com>
2018-10-21 21:48:59 +0300

    First lesson calculator rewrited

---
+++ src/com/startjava/lesson_1/calculator/Calculator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		 System.out.println("Введите первое число:");
+		 int firstNum = scan.nextInt();
+
+		 System.out.println("Введите знак математического действия:");
+		 char mathSign = scan.next().charAt(0);
+
+		 System.out.println("Введите второе число:");
+		 int secondNum = scan.nextInt();
+
+		 System.out.print(firstNum + " " + mathSign + " " + secondNum + " = ");
+
+		 if (mathSign == '+') {
+		 	System.out.println(firstNum + secondNum);
+		 } else if (mathSign == '-') {
+		 	System.out.println(firstNum - secondNum);
+		 } else if (mathSign == '*') {
+		 	System.out.println(firstNum * secondNum);
+		 } else if (mathSign == '/') {
+		 	System.out.println((double)firstNum / (double)secondNum);
+		 } else if (mathSign == '%') {
+		 	System.out.println(firstNum % secondNum);
+		 } else if (mathSign == '^') {
+		 	int result = 1;
+		 	for (int i = 1; i <= secondNum; i++) {
+		 		result *= firstNum;
+		 	}
+		 	System.out.println(result);
+		 } else {
+		 	System.out.println("Normal person: \"Stop it\" ");
+		 }
+	}
+}
